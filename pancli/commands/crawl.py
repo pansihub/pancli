6b90fdf36c77357506a0acbb4776859eaf50ef3b
KevenLi <pbleester@gmail.com>
2020-07-19 14:32:34 +0800

    crawl --package argument

--- pancli/commands/crawl.py
+++ pancli/commands/crawl.py
@@ -3,10 +3,12 @@ from . import CommandBase
 class CrawlCommand(CommandBase):
     def add_arguments(self, parser):
         parser.add_argument('spider', nargs='?')
+        parser.add_argument('--package')
 
     def run(self, args):
         from ..runner import activate_project, execute
-        settings = activate_project()
+        package = args.package
+        settings = activate_project(package)
         spider_name = args.spider
         if not spider_name:
             print('no spider name spicified')
