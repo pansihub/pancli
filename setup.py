e37fda822289de1441d3677c83e78c0cb1436ceb
KevenLi <pbleester@gmail.com>
2020-07-21 19:10:47 +0800

    version v0.1.0

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 import os
+from os.path import join, dirname
 import io
 from setuptools import setup, find_packages
 
 
+with open(join(dirname(__file__), 'pancli/VERSION'), 'rb') as f:
+    version = f.read().decode('ascii').strip()
+
+
 setup(
     name='pancli',
-    version='0.0.3',
+    version=version,
     author='kevenli',
     author_email='pbleester@gmail.com',
     url='https://github.com/pansihub/pancli',
-    packages=find_packages(),
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    include_package_data=True,
+    zip_safe=False,
     description='pansihub command-line tools.',
     entry_points={
         'console_scripts': [
