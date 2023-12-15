c987f3e6eb32e3e72cd966775ba1b19aff9c7d4e
KevenLi <pbleester@gmail.com>
2023-12-15 23:14:11 +0800

    test org_example project setup.py

---
+++ tests/org_example/setup.py
@@ -0,0 +1,9 @@
+# Automatically created by: scrapydd
+from setuptools import setup, find_packages
+setup(
+    name         = 'org_example',
+    version      = '1.0',
+    packages     = find_packages(),
+    entry_points = {'scrapy': ['settings = org_example.settings']},
+    install_requires = [],
+)
